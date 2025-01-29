d822536b807cea51715d5183c81acbce3c6fd26c
Rapiro <rapiromail@gmail.com>
2025-01-29 16:11:14 +0400

    Добавил Lombok в сущность юзера чтобы уменьшить кол-во кода (убрал геттеры и сеттеры)

--- src/main/java/jm/task/core/jdbc/model/User.java
+++ src/main/java/jm/task/core/jdbc/model/User.java
@@ -1,9 +1,14 @@
 package jm.task.core.jdbc.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Column;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+@Setter
+@Getter
 @Table
 public class User {
     @Id
@@ -25,38 +30,6 @@ public class User {
     public User(String name, String lastName, Byte age) {
         this.name = name;
         this.lastName = lastName;
-        this.age = age;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public Byte getAge() {
-        return age;
-    }
-
-    public void setAge(Byte age) {
         this.age = age;
     }
 
