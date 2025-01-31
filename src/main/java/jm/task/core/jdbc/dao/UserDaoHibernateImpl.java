f907d86d349d901718949b9e9a1622add9bb1b14
Rapiro <rapiromail@gmail.com>
2025-01-31 16:57:43 +0400

    Здесь будет реализация с использованием Hibernate

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -3,7 +3,7 @@ package jm.task.core.jdbc.dao;
 import jm.task.core.jdbc.model.User;
 
 import java.util.List;
-
+// Тут будет реализация
 public class UserDaoHibernateImpl implements UserDao {
     public UserDaoHibernateImpl() {
 
